b2a832904c9b195c8b6d2751046f151e38604a0e
yzw <415763469@qq.com>
2018-06-25 18:28:29 +0800

    a

--- seller/js/webjs/account_information.js
+++ seller/js/webjs/account_information.js
@@ -1,9 +1,21 @@
 $(function(){
-    alert(1)
     var sc_sub_nav = $('.sc-sub-nav');
-    var sc_hover_nav = $('.sc_hover_nav');
-    sc_hover_nav.mouseover(function(){
-        alert('a')
-        sc_sub_nav.addClass('hide');
-    })
+    var sc_hover_nav = $('.sc-hover-nav');
+    console.log(sc_hover_nav)
+   
+    for(let i=0;i<sc_hover_nav.length;i++){
+         $('.sc-hover-nav').eq([i]).attr('data-list',i)
+         sc_hover_nav[i].addEventListener('mouseover',function(){
+            $('.sc-hover-nav').eq([this.dataset.list])
+            .find('.sc-nav-arrow').eq(0).css({opacity:1,visibility:'visible'});
+            $('.sc-hover-nav').eq([this.dataset.list])
+            .find('.sc-sub-nav').eq(0).css({display:'block'});
+         })
+         sc_hover_nav[i].addEventListener('mouseout',function(){
+            $('.sc-hover-nav').eq([this.dataset.list])
+            .find('.sc-nav-arrow').eq(0).css({opacity:0,visibility:'hidden'});
+            $('.sc-hover-nav').eq([this.dataset.list])
+            .find('.sc-sub-nav').eq(0).css({display:'none'});
+         })
+    }
 })
