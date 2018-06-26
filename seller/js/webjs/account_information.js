88da3876699f7422d89abb4bfa6145387c95173f
yzw <415763469@qq.com>
2018-06-26 11:13:08 +0800

    submit for move

--- seller/js/webjs/account_information.js
+++ seller/js/webjs/account_information.js
@@ -2,26 +2,14 @@ window.onload = function() {
     //新建弹窗，然后里面的样式你自己去写
     var  what = document.getElementById('what')
     what.onclick = openwindow();
-  function openwindow() {
-    let width = document.documentElement.clientWidth;//获取当前屏幕的宽度，这个宽度是除去控制台的宽度，body的宽度
-    let height = window.screen.height;//获取当前显示器的高度，浏览器
-    // console.log(width,height)
-    let left = (width - 350) / 2;
-    let top = (height - 458) / 3;
-    // console.log(left,top)
-    window.open(
-      "./shared.html",
-      "_blank",
-      `width=350,height=458,left=${left},top=${top},menubar=no,toolbar=no,location=no,directories=no,status=no,scrollbars=yes,resizable=yes`
-    );
-  }
+  
 
     function openwindow() {
       let width = document.documentElement.clientWidth; //获取当前屏幕的宽度，这个宽度是除去控制台的宽度，body的宽度
       let height = window.screen.height; //获取当前显示器的高度，浏览器
       let left = (width - 350) / 2;
       let top = (height - 458) / 3;
-      console.log(left,top)
+     // console.log(left,top)
       /*window.open(
         "./shared.html",
         "_blank",
@@ -54,7 +42,6 @@ window.onload = function() {
     }
     getSurport.onclick = function () {
       show ?spaui.style.right = - divWidth +'px' : spaui.style.right = '0px';
-      console.log(spaui.style.right)
       show = !show;
     }
     
