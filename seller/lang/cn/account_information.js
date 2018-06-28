7e63d6a1e0728a4acd45dfc9ce2d27f2b7c867c2
yzw <415763469@qq.com>
2018-06-28 18:30:06 +0800

    no message

--- seller/lang/cn/account_information.js
+++ seller/lang/cn/account_information.js
@@ -3,6 +3,30 @@
         title: ""
     }
     body: {
-        
+        LeftNav: {
+            NavCatalogue: '目录',
+            NavInventory: '库存',
+            NavOrder: "订单",
+            NavAdvertisement: "广告",
+            NavStore: "店铺",
+            NavDataReport: "数据报告",
+            NavAchievements: "绩效",
+            
+        },
+        RightNav: {
+            NavBuyerNews: "买家消息",
+            NavHelp: "帮助",
+            NavSetUp: "设置",
+            shoper:{
+                a:''
+            }
+        },
+        saleShopper: {
+            shopper:"卖家账户信息",
+            options:"设置"
+        }
     }
+}
+a:{
+
 }
