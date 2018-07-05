4be92921f653d6986b7ee95ada809e8c0d0df142
yzw <415763469@qq.com>
2018-07-05 10:01:16 +0800

    no message

--- seller/js/webjs/deposit_method_distribution.js
+++ seller/js/webjs/deposit_method_distribution.js
@@ -1,7 +1,3 @@
 window.onload = function() {
-  $(".update_deposit").click(function() {
-    console.log("1");
-    $(".table_one").css({ display: "none" });
-    $(".table_two").css({ display: "block" });
-  });
+  
 };
