61d88a72f100960d7be8f2ca407e99c66f3bbaa3
yzw <415763469@qq.com>
2018-07-11 18:27:57 +0800

    no message

--- seller/js/webjs/company_name_tax.js
+++ seller/js/webjs/company_name_tax.js
@@ -1,12 +1,30 @@
-window.onload=function(){
-    $('.tip').hover(function () { 
+window.onload = function () {
+    $('.tip').hover(function () {
         $('.personal').show()
-     },function () {
+    }, function () {
         $('.personal').hide()
-     })
-     $('.tip2').hover(function () { 
+    })
+    $('.tip2').hover(function () {
         $('.business').show()
-     },function () {
+    }, function () {
         $('.business').hide()
-     })
+    })
+    function chooseClass (btn1,btn2,className){
+        $(btn1).click(function () { 
+            $(btn1).addClass(className)
+            $(btn2).removeClass(className)
+         })
+    }
+    // $('#select').hover(function(){
+    //     $('#select').css({
+    //         backgroundColor:' #dcdfe3',
+    //     })
+    // })
+    chooseClass('.a-choose-yes','.a-choose-no','a-button-choose')
+    chooseClass('.a-choose-no','.a-choose-yes','a-button-choose')
+    chooseClass('.a-choose-business','.a-choose-persinal','a-button-choose')
+    chooseClass('.a-choose-persinal','.a-choose-business','a-button-choose')
+
+    
 }
+
---
+++ seller/js/webjs/prompt.js
@@ -0,0 +1,36 @@
+
+    function addwarn(target,type,text){
+        /*
+        * @target 插入的目标节点
+        * @type图片类型，1为info i，2是error ！
+        * @text  提示文字
+        */
+        let imgtype = '';
+        type == 1?imgtype="info":imgtype="error";
+        let div = document.createElement('div');
+        div.setAttribute('class','a-row myWarn')
+        div.innerHTML = `<div class="a-column ">
+                                <div  class="a-box a-alert-inline a-alert-inline-${imgtype}" >
+                                    <div class="a-box-inner a-alert-container">
+                                        <i class="a-icon a-icon-alert"></i>
+                                        <div class="a-alert-content">
+                                            ${text}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        `
+        if(document.getElementById(target).getElementsByTagName('input')[0]){
+          //input框
+          $(`#${target} input`).after(div)
+        }else if(document.getElementById(target).getElementsByTagName('select')[0]){
+          //select下拉框
+          $(`#${target} select`).after(div)
+          
+        }else{
+          //其他处理
+        }
+      }
+    addwarn('full_name',2,'必填字段') 
+    addwarn('tel',2,'必填字段') 
+
